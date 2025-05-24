90bc2106d93a3d8ff4bb8f6058df17c3e42a73f6
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2025-05-24 12:55:06 +0300

    create NewDocumentPage widget

---
+++ lib/pages/new_document_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewDocumentPage extends StatelessWidget {
+  const NewDocumentPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
