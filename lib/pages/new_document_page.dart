72cd1d8bbdbf80e0ebf8c0aebc51568665fb9a2f
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2025-05-24 15:31:07 +0300

    implement NewDocumentPage widget

--- lib/pages/new_document_page.dart
+++ lib/pages/new_document_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:medrocket_home_task_1/utility/colors.dart';
+import 'package:medrocket_home_task_1/widgets/new_document_form.dart';
 
 class NewDocumentPage extends StatelessWidget {
   const NewDocumentPage({super.key});
@@ -25,8 +26,8 @@ class NewDocumentPage extends StatelessWidget {
       ),
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsetsGeometry.only(left: 16.0, right: 16.0, top: 16.0),
-          child: const Placeholder()
+          padding: EdgeInsetsGeometry.only(left: 16.0, right: 16.0, top: 32.0),
+          child: NewDocumentForm(),
         )
       ),
     );
