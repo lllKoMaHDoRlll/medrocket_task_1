bda6a92d5eaa1e95c7bab3c4567ce6c1457dbd39
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2025-05-24 13:39:40 +0300

    implement app bar of NewDocumentPage

--- lib/pages/new_document_page.dart
+++ lib/pages/new_document_page.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:medrocket_home_task_1/utility/colors.dart';
 
 class NewDocumentPage extends StatelessWidget {
   const NewDocumentPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.arrow_back),
+          color: AppColors.textPrimary,
+        ),
+        title: const Text(
+          "Новый документ",
+          style: TextStyle(
+            fontWeight: FontWeight.w700,
+            color: AppColors.textPrimary
+          ),
+        ),
+        shadowColor: AppColors.textSecondary,
+        elevation: 0.5,
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsetsGeometry.only(left: 16.0, right: 16.0, top: 16.0),
+          child: const Placeholder()
+        )
+      ),
+    );
   }
 }
