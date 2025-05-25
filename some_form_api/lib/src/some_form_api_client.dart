16c72bbb47c0229298e9e4a3e1862ae8234bcfc4
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2025-05-25 16:13:38 +0300

    add isMocked flag for getSomeFormData api method

--- some_form_api/lib/src/some_form_api_client.dart
+++ some_form_api/lib/src/some_form_api_client.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:http/http.dart' as http;
 import 'package:some_form_api/some_form_api.dart';
@@ -8,6 +9,7 @@ class SomeFormDataRequestFailure implements Exception {}
 
 class SomeFormDataNotFoundFailure implements Exception {}
 
+const bool isMocked = true;
 
 class SomeFormApiClient {
   SomeFormApiClient({http.Client? httpClient})
@@ -18,6 +20,14 @@ class SomeFormApiClient {
   final http.Client _httpClient;
 
   Future<SomeFormData> getSomeFormData() async {
+    if (isMocked) {
+      await Future.delayed(Duration(seconds: 1));
+      final rng = Random();
+      final mediumProgress = rng.nextDouble();
+      final randomizedProgressList = [0.0, mediumProgress - (mediumProgress % 0.01), 1.0][rng.nextInt(3)];
+      return SomeFormData(progress: randomizedProgressList);
+    }
+
     final someFormDataRequest = Uri.https(
       _baseUrl,
       '/some/endpoint',
